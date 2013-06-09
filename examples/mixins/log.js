5902dfd33782c9ddb1a9926ca26d75e840dd3f3c
Alexander Farennikov <alex@teradek.com>
2013-06-09 11:29:49 -0700

    Changed the way mixin extends model prototype

--- examples/mixins/log.js
+++ examples/mixins/log.js
@@ -1,23 +1,27 @@
+var _ = require('underscore');
+
 module.exports = {
     initialize: function() {
         this.on('afterInitialize', function() {
             console.log('prometheus mixins/log.js: afterInitialize');
         });
 
-        this.__proto__.log = function(data) {
-            var coll = this._name.toLowerCase().replace(/model/g, '') + '_log',
-                def  = deferred(),
-                payload;
+        _.extend(this.__proto__, {
+            log: function(data) {
+                var coll = this._name.toLowerCase().replace(/model/g, '') + '_log',
+                    def  = deferred(),
+                    payload;
 
-            payload = _.extend(data || {}, {
-                ts: utils.now(13)
-            });
+                payload = _.extend(data || {}, {
+                    ts: utils.now(13)
+                });
 
-            console.log('prometheus mixins/log.js log to %s :', coll, payload);
+                console.log('prometheus mixins/log.js log to %s :', coll, payload);
 
-            def.resolve(payload);
+                def.resolve(payload);
 
-            return def.promise;
-        }
+                return def.promise;
+            }
+        });
     }
 };
